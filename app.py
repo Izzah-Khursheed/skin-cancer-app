2946cf27cc4f1c66c12a87ab95710261c7c02eb2
Izzah Khursheed <izzahkhursheed1800@gmail.com>
2025-05-31 20:56:10 +0500

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ with tabs[0]:
     
     if uploaded_file is not None:
         image = Image.open(uploaded_file).convert("RGB")
-        st.image(image, caption="Uploaded Image", use_column_width=True)
+        st.image(image, caption="Uploaded Image", use_container_width=True)
         st.write("🔍 Making prediction...")
 
         # Run prediction
