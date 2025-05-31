59dc2cd79ba78916255b4563d6e25ca63cbb7923
Izzah Khursheed <izzahkhursheed1800@gmail.com>
2025-05-31 22:22:53 +0500

    Update treatment_info.py

--- utils/treatment_info.py
+++ utils/treatment_info.py
@@ -198,12 +198,9 @@ def get_treatment_info(cancer_type):
 
     # Format each sentence/line as bullet points
     # Split by '.' to get separate points, filter empty, strip spaces
-    # def format_as_bullets(lines):
-    #     # lines is already a list of strings, format each as bullet point
-    #     return '\n'.join([f"• {line}" for line in lines])
-
     def format_as_bullets(lines):
-        return '<ul>' + ''.join([f"<li>{line}</li>" for line in lines]) + '</ul>'
+        # lines is already a list of strings, format each as bullet point
+        return '\n'.join([f"• {line}" for line in lines])
 
     treatment_formatted = format_as_bullets(entry['treatment'])
     precautions_formatted = format_as_bullets(entry['precautions'])
